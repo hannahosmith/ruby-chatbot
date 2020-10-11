4e58f69e69876a2c113732782ada419d78f6bda3
hannahsmith <fueledbyskwinklotez@gmail.com>
2020-10-10 23:29:09 -0700

    Adds additional if statement to send response to chat room

--- chatbot.rb
+++ chatbot.rb
@@ -22,6 +22,11 @@ while message = socket.gets do
     socket.puts "PONG #{server}"
   end
 
+  if message.downcase.match('hey')
+    puts "hey there"
+    socket.puts "PRIVMSG #{channel} :hey there"
+  end
+
   if message.downcase.match('how are you?')
     puts "I'm fine boy. How r u"
     socket.puts "PRIVMSG #{channel} :I'm fine boy. How r u"
